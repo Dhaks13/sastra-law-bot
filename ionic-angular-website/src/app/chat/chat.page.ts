11be12c28ca62852bf4b05114591469f8e1ff6c1
Dhakshin A V <avdhakshin1354@gmail.com>
2025-08-22 14:07:27 +0530

    minor fix

--- ionic-angular-website/src/app/chat/chat.page.ts
+++ ionic-angular-website/src/app/chat/chat.page.ts
@@ -25,19 +25,19 @@ export class ChatPage {
 
   constructor(public loading: LoadingService ,private cookieService: CookieService ,private route: ActivatedRoute, private router: Router,private apiService: ApiService) {
     this.loading.setLoading(true);
-    // this.username = this.getUsernameFromCookie();
-    // if(this.getUsernameFromCookie()==''){
-    //     this.router.navigate(['/home']);
-    // }
+    this.username = this.getUsernameFromCookie();
+    if(this.getUsernameFromCookie()==''){
+        this.router.navigate(['/home']);
+    }
     this.loading.setLoading(false);
   }
 
   ngOnInit() {
     this.loading.setLoading(true);
     this.username = this.getUsernameFromCookie();
-    // if(this.getUsernameFromCookie()==''){
-    //     this.router.navigate(['/home']);
-    // }
+    if(this.getUsernameFromCookie()==''){
+        this.router.navigate(['/home']);
+    }
     this.loading.setLoading(false);
   } 
   
